17db0c98e8c25f72c76cc44cf982cf77197559ec
iara.pou <iarapou21@hotmail.com>
2025-09-03 21:51:36 -0300

    Agrega navbar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,11 @@
-export default function Index()
-{
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-3xl font-bold text-blue-600">Landing Page</h1>
-        </div>
-    )
+import Header from "../components/Header/Header";
+export default function Index() {
+  return (
+    <>
+      <Header></Header>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-600">Landing Page</h1>
+      </div>
+    </>
+  );
 }
