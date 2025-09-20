80296c6150c1fffebbc58c4c39479ffb0a7c9c3d
iara.pou <iarapou21@hotmail.com>
2025-09-20 18:11:42 -0300

    Agrega responsive y migra a material página about us

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -12,6 +12,22 @@ const Carousel = ({ items }) => {
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
