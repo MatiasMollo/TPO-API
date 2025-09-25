190cbd2c099f8bcdd01f8ea014b7bb947fedf266
iara.pou <iarapou21@hotmail.com>
2025-09-24 21:13:12 -0300

    Arregla altura imagen

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -19,129 +19,131 @@ export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div className="d-flex justify-content-center min-vh-100">
-      <Grid container sx={{ minHeight: "100vh" }} alignItems="stretch">
-        <Grid item size={{ xs: 0, md: 7, xl: 8 }} sx={{ height: "100%" }}>
-          <Box
-            component="img"
-            src="/src/assets/clients.jpg"
-            alt="Imagen de clientes"
-            sx={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-            }}
-          />
-        </Grid>
-        <Grid
-          item
-          spacing={2}
-          size={{ xs: 12, md: 5, xl: 4 }}
+    <Grid
+      container
+      sx={{ minHeight: "100vh", overflow: "hidden" }} 
+    >
+      <Grid item size={{ xs: 0, md: 7, xl: 8 }} sx={{ height: "100%" }}>
+        <Box
+          component="img"
+          src="/src/assets/clients.jpg"
+          alt="Imagen de clientes"
           sx={{
-            bgcolor: "white",
-            boxShadow: 3,
-            p: 4,
-            minHeight: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            width: "100%",
+            minHeight: "100vh", 
+            objectFit: "cover",
+            display: "block",
           }}
-          justifyContent={{ xs: "start", md: "center" }}
-        >
-          <Box
-            maxWidth={"400px"}
-            display={"flex"}
-            flexDirection={"column"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            p={{ xs: 1, md: 0 }}
-          >
-            <Typography variant="h2" marginTop={5}>
-              ¡Bienvenido!
-            </Typography>
-            <Typography variant="body1" color="text.secondary" gutterBottom>
-              Por favor, completá tus datos para ingresar
-            </Typography>
-            <form>
-              <TextField
-                fullWidth
-                margin="normal"
-                id="documento"
-                label="Número de documento"
-                variant="outlined"
-              />
-
-              {/* Contraseña */}
-              <TextField
-                fullWidth
-                margin="normal"
-                id="password"
-                label="Contraseña"
-                type={showPassword ? "text" : "password"}
-                variant="outlined"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        onClick={() => setShowPassword(!showPassword)}
-                        edge="end"
-                        aria-label="toggle password visibility"
-                      >
-                        <EyeIcon width={20} />
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-
-              {/* Recordar */}
-              <FormControlLabel
-                control={<Checkbox id="recordar" />}
-                label="Recordar el número de documento"
-              />
-
-              {/* Botón */}
-              <Box display="flex" justifyContent="center" mt={2}>
-                <Link to="/citas" style={{ textDecoration: "none" }}>
-                  <Button
-                    variant="contained"
-                    sx={{
-                      backgroundColor: "#01819d",
-                      mt: 2,
-                      px: 4,
-                      py: 1,
-                      display: "block",
-                    }}
-                  >
-                    Ingresar
-                  </Button>
-                </Link>
-              </Box>
-            </form>
-            <Card
-              variant="outlined"
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                mt: 4,
-                p: 2,
-                bgcolor: "grey.50",
-              }}
-            >
-              <Box ml={2}>
-                <InformationCircleIcon width="24" className="text-primary" />
-                <Typography variant="body2" fontWeight="bold">
-                  No compartas tus claves.
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Nunca te solicitaremos esta información por redes sociales,
-                  teléfono o email.
-                </Typography>
-              </Box>
-            </Card>
-          </Box>
-        </Grid>
+        />
       </Grid>
-    </div>
+      <Grid
+        item
+        spacing={2}
+        size={{ xs: 12, md: 5, xl: 4 }}
+        sx={{
+          bgcolor: "white",
+          boxShadow: 3,
+          p: 4,
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        justifyContent={{ xs: "start", md: "center" }}
+      >
+        <Box
+          maxWidth={"400px"}
+          display={"flex"}
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          p={{ xs: 1, md: 0 }}
+        >
+          <Typography variant="h2" marginTop={5}>
+            ¡Bienvenido!
+          </Typography>
+          <Typography variant="body1" color="text.secondary" gutterBottom>
+            Por favor, completá tus datos para ingresar
+          </Typography>
+          <form>
+            <TextField
+              fullWidth
+              margin="normal"
+              id="documento"
+              label="Número de documento"
+              variant="outlined"
+            />
+
+            {/* Contraseña */}
+            <TextField
+              fullWidth
+              margin="normal"
+              id="password"
+              label="Contraseña"
+              type={showPassword ? "text" : "password"}
+              variant="outlined"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={() => setShowPassword(!showPassword)}
+                      edge="end"
+                      aria-label="toggle password visibility"
+                    >
+                      <EyeIcon width={20} />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+
+            {/* Recordar */}
+            <FormControlLabel
+              control={<Checkbox id="recordar" />}
+              label="Recordar el número de documento"
+            />
+
+            {/* Botón */}
+            <Box display="flex" justifyContent="center" mt={2}>
+              <Link to="/citas" style={{ textDecoration: "none" }}>
+                <Button
+                  variant="contained"
+                  sx={{
+                    backgroundColor: "#01819d",
+                    mt: 2,
+                    px: 4,
+                    py: 1,
+                    display: "block",
+                  }}
+                >
+                  Ingresar
+                </Button>
+              </Link>
+            </Box>
+          </form>
+          <Card
+            variant="outlined"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              mt: 4,
+              p: 2,
+              bgcolor: "grey.50",
+            }}
+          >
+            <Box ml={2}>
+              <InformationCircleIcon width="24" className="text-primary" />
+              <Typography variant="body2" fontWeight="bold">
+                No compartas tus claves.
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Nunca te solicitaremos esta información por redes sociales,
+                teléfono o email.
+              </Typography>
+            </Box>
+          </Card>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
