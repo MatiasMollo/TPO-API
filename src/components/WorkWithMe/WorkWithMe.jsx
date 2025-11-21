d123176908430729b13ef0e8db0e42f104a008b4
iara.pou <iarapou21@hotmail.com>
2025-11-21 18:56:34 -0300

    Integra formulario de citas al back

--- src/components/WorkWithMe/WorkWithMe.jsx
+++ src/components/WorkWithMe/WorkWithMe.jsx
@@ -12,43 +12,128 @@ import {
   Snackbar,
   Alert,
 } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import "dayjs/locale/es";
+import axios from "axios";
+import dayjs from "dayjs";
 
 const WorkWithMe = () => {
-  const availableTimes = {
-    "2025-10-03": ["09:00", "10:00", "14:00"],
-    "2025-10-04": ["11:00", "15:00", "16:00"],
-    "2025-10-05": ["09:30", "13:00", "16:30"],
-  };
-
-  const obrasSociales = ["OSDE", "AMFFA", "PAMI"];
-
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [selectedTime, setSelectedTime] = useState("");
-  const [obraSocial, setObraSocial] = useState("");
-  const [nombre, setNombre] = useState("");
-  const [telefono, setTelefono] = useState("");
-  const [email, setEmail] = useState("");
-  const [motivo, setMotivo] = useState("");
-
+  const [loading, setLoading] = useState(false);
+  const [inputs, setInputs] = useState({
+    nombre: "",
+    telefono: "",
+    email: "",
+    obra_social: "",
+    fecha: null,
+    hora: "",
+    motivo: "",
+  });
+  const [error, setError] = useState("");
+  const [warning, setWarning] = useState("");
   const [openSnackbar, setOpenSnackbar] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [obrasSociales, setObrasSociales] = useState([]);
+  const [citasOcupadas, setCitasOcupadas] = useState([]);
 
-    setNombre("");
-    setTelefono("");
-    setEmail("");
-    setObraSocial("");
-    setSelectedDate(null);
-    setSelectedTime("");
-    setMotivo("");
-    setOpenSnackbar(true);
-  };
+  function resetFormulario() {
+    setInputs({
+      nombre: "",
+      telefono: "",
+      email: "",
+      obra_social: "",
+      fecha: null,
+      hora: "",
+      motivo: "",
+    });
+  }
+
+  async function getObrasSociales() {
+    try {
+      const response = await axios.get("http://localhost:3000/api/obraSocial");
+      setObrasSociales(response.data.obras_sociales);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  //get de citas disponibles ocupadas. Si se ocupa, lo quito de los horarios disponibles
+  async function getCitas() {
+    try {
+      const response = await axios.get("http://localhost:3000/api/citas");
+      console.log("Obtuve citas:", response.data);
+      setCitasOcupadas(response.data.citas);
+    } catch (err) {
+      setError(
+        "Tuvimos un problema obteniendo las citas reservadas. Es posible que algunos horarios mostrados ya no estén disponibles."
+      );
+      console.log(err);
+    }
+  }
+
+  function generarHorarios() {
+    const horas = [];
+    for (let h = 9; h <= 17; h++) {
+      const hourStr = h.toString().padStart(2, "0") + ":00";
+      horas.push(hourStr);
+    }
+    return horas;
+  }
+
+  function getHorariosDisponibles(fechaSeleccionada) {
+    if (!fechaSeleccionada) return [];
+
+    const fecha = fechaSeleccionada.format("YYYY-MM-DD");
+    const todos = generarHorarios();
+
+    const ocupadasEnFecha = citasOcupadas
+      .filter((cita) => cita.fecha === fecha)
+      .map((cita) => cita.hora.slice(0, 5)); // "HH:MM"
+
+    const disponibles = todos.filter((hora) => !ocupadasEnFecha.includes(hora));
+
+    return disponibles;
+  }
+
+  useEffect(() => {
+    getObrasSociales();
+    getCitas();
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(inputs);
+
+    setLoading(true);
+    setError("");
+
+    try {
+      //convierto fecha a formato YYYY-MM-DD
+      const response = await axios.post("http://localhost:3000/api/citas", {
+        ...inputs,
+        fecha: inputs.fecha.format("YYYY-MM-DD"),
+      });
+
+      console.log("Cita creada:", response.data);
+
+      //vuelvo a obtener citas para actualizar horarios disponibles
+      getCitas();
+      resetFormulario();
+      setOpenSnackbar(true);
+    } catch (err) {
+      console.log(err);
+      if (err.response.data.error) setError(err.response.data.error);
+      else if (err.response.data.message) setError(err.response.data.message);
+      else
+        setError(
+          "Ocurrió un error a la hora de reservar la cita. Por favor, reintente más tarde."
+        );
+    } finally {
+      setLoading(false);
+    }
+  }
 
   const handleCloseSnackbar = (_, reason) => {
     if (reason === "clickaway") return;
@@ -84,6 +169,31 @@ const WorkWithMe = () => {
           Reservá un turno conmigo, completando los siguientes datos:
         </Typography>
 
+        {/* Mensajes error */}
+        {warning && (
+          <Alert
+            severity="warning"
+            sx={{
+              width: "92%",
+              margin: "1em 0em",
+            }}
+          >
+            {error}
+          </Alert>
+        )}
+
+        {error && (
+          <Alert
+            severity="error"
+            sx={{
+              width: "92%",
+              margin: "1em 0em",
+            }}
+          >
+            {error}
+          </Alert>
+        )}
+
         <Box maxWidth={{ xs: "100%", md: "530px" }}>
           <form onSubmit={handleSubmit}>
             <Box
@@ -97,93 +207,121 @@ const WorkWithMe = () => {
                 label="Nombre y apellido"
                 variant="outlined"
                 fullWidth
-                value={nombre}
-                onChange={(e) => setNombre(e.target.value)}
+                value={inputs.nombre}
+                onChange={(e) =>
+                  setInputs({ ...inputs, nombre: e.target.value })
+                }
               />
               <TextField
                 label="Teléfono"
                 variant="outlined"
                 fullWidth
-                value={telefono}
                 type="number"
-                onChange={(e) => setTelefono(e.target.value)}
+                value={inputs.telefono}
+                onChange={(e) =>
+                  setInputs({ ...inputs, telefono: e.target.value })
+                }
               />
               <TextField
-                label="E-mail"
+                label="Email"
                 variant="outlined"
                 fullWidth
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                value={inputs.email}
+                onChange={(e) =>
+                  setInputs({ ...inputs, email: e.target.value })
+                }
               />
-
-              {/* Select obra social */}
               <FormControl fullWidth>
                 <InputLabel>Obra social</InputLabel>
                 <Select
-                  value={obraSocial}
                   label="Obra social"
-                  onChange={(e) => setObraSocial(e.target.value)}
+                  value={inputs.obra_social}
+                  onChange={(e) => {
+                    console.log(e.target);
+                    setInputs({ ...inputs, obra_social: e.target.value });
+                  }}
                 >
                   {obrasSociales.map((obra) => (
-                    <MenuItem key={obra} value={obra}>
-                      {obra}
+                    <MenuItem key={obra.id} value={obra.id}>
+                      {obra.nombre}
                     </MenuItem>
                   ))}
                 </Select>
               </FormControl>
 
-              {/* Input fecha */}
               <LocalizationProvider
                 dateAdapter={AdapterDayjs}
                 adapterLocale="es"
               >
                 <DatePicker
                   label="Seleccioná una fecha"
-                  value={selectedDate}
-                  onChange={(newValue) => {
-                    setSelectedDate(newValue);
-                    setSelectedTime(""); // reset hora al cambiar fecha
+                  value={inputs.fecha}
+                  onChange={(newDate) => {
+                    setInputs({
+                      ...inputs,
+                      fecha: newDate,
+                      hora: "",
+                    });
+                  }}
+                  shouldDisableDate={(day) => {
+                    const weekday = day.day(); // 0 domingo, 6 sábado
+                    if (weekday === 0 || weekday === 6) return true;
+                    if (
+                      day.isBefore(dayjs(), "day") ||
+                      day.isSame(dayjs(), "day")
+                    )
+                      return true;
+                    return false;
                   }}
                 />
               </LocalizationProvider>
 
-              {/* Select de horas */}
               <FormControl
                 fullWidth
-                disabled={!selectedDate}
+                disabled={!inputs.fecha}
                 sx={{ marginBottom: "1em" }}
               >
                 <InputLabel>Hora</InputLabel>
                 <Select
-                  value={selectedTime}
                   label="Hora"
-                  onChange={(e) => setSelectedTime(e.target.value)}
+                  value={inputs.hora}
+                  onChange={(e) => {
+                    setInputs({
+                      ...inputs,
+                      hora: e.target.value,
+                    });
+                  }}
                 >
-                  {selectedDate &&
-                  availableTimes[selectedDate.format("YYYY-MM-DD")] ? (
-                    availableTimes[selectedDate.format("YYYY-MM-DD")].map(
-                      (time) => (
+                  {inputs.fecha ? (
+                    getHorariosDisponibles(inputs.fecha).length > 0 ? (
+                      getHorariosDisponibles(inputs.fecha).map((time) => (
                         <MenuItem key={time} value={time}>
                           {time}
                         </MenuItem>
-                      )
+                      ))
+                    ) : (
+                      <MenuItem disabled>No hay horas disponibles</MenuItem>
                     )
                   ) : (
-                    <MenuItem disabled>No hay horas disponibles</MenuItem>
+                    <MenuItem disabled>Elegí una fecha</MenuItem>
                   )}
                 </Select>
               </FormControl>
             </Box>
 
-            {/* Textarea motivo consulta */}
             <TextField
               label="Motivo de consulta"
               multiline
               rows={4}
               variant="outlined"
               fullWidth
-              value={motivo}
-              onChange={(e) => setMotivo(e.target.value)}
+              value={inputs.motivo}
+              onChange={(e) => {
+                setInputs({
+                  ...inputs,
+                  motivo: e.target.value,
+                });
+              }}
             />
 
             <Button
@@ -196,8 +334,9 @@ const WorkWithMe = () => {
                 marginTop: "1em",
                 animation: "none",
               }}
+              disabled={loading}
             >
-              Enviar
+              {loading ? "Cargando..." : "Enviar solicitud"}
             </Button>
           </form>
         </Box>
@@ -224,7 +363,7 @@ const WorkWithMe = () => {
         />
       </Grid>
 
-      {/* Snackbar flotante */}
+      {/* Mensaje éxito */}
       <Snackbar
         open={openSnackbar}
         autoHideDuration={2000}
