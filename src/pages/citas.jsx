c7d3a765e52762eefa5e4642c73b87b7624b932c
iara.pou <iarapou21@hotmail.com>
2025-11-21 20:23:47 -0300

    Recupera citas de BBDD

--- src/pages/citas.jsx
+++ src/pages/citas.jsx
@@ -18,40 +18,31 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { useState } from "react";
-
-// Repositorio temporal hasta conectar DB
-const citasIniciales = [
-  {
-    paciente: "María González",
-    fecha: "14/1/2024",
-    hora: "09:00",
-    especialidad: "Cardiología",
-    estado: "Sin confirmar",
-    contacto: "+34 666 123 456",
-    email: "maria.gonzalez@email.com",
-    notas: "Primera consulta",
-  },
-  {
-    paciente: "Carlos Rodríguez",
-    fecha: "14/1/2024",
-    hora: "10:30",
-    especialidad: "Dermatología",
-    estado: "Confirmada",
-    contacto: "+34 677 987 654",
-    email: "carlos.rodriguez@email.com",
-    notas: "Revisión lunar",
-  },
-];
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Citas() {
-  const [citas, setCitas] = useState(citasIniciales);
+  const [citas, setCitas] = useState([]);
+
+  async function getCitas() {
+    try {
+      const response = await axios.get("http://localhost:3000/api/citas");
+      console.log("Obtuve citas:", response.data);
+      setCitas(response.data.citas);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   const confirmarCita = (index) => {
     const nuevasCitas = [...citas];
     nuevasCitas[index].estado = "Confirmada";
     setCitas(nuevasCitas);
   };
+
+  useEffect(() => {
+    getCitas();
+  }, []);
 
   return (
     <Container
@@ -94,17 +85,9 @@ export default function Citas() {
             <InputLabel>Estado</InputLabel>
             <Select label="Estado" defaultValue="">
               <MenuItem value="">Todos los estados</MenuItem>
-              <MenuItem value="Sin confirmar">Sin confirmar</MenuItem>
+              <MenuItem value="pendiente">Sin confirmar</MenuItem>
               <MenuItem value="Confirmada">Confirmada</MenuItem>
               <MenuItem value="Realizada">Realizada</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl size="small" sx={{ minWidth: 200 }}>
-            <InputLabel>Especialidad</InputLabel>
-            <Select label="Especialidad" defaultValue="">
-              <MenuItem value="">Todas las especialidades</MenuItem>
-              <MenuItem value="Cardiología">Cardiología</MenuItem>
-              <MenuItem value="Dermatología">Dermatología</MenuItem>
             </Select>
           </FormControl>
           <FormControl size="small" sx={{ minWidth: 200 }}>
@@ -122,7 +105,6 @@ export default function Citas() {
           alignItems="center"
           gap={1}
         >
-          {/* <CalendarMonth fontSize="small" /> */}
           Citas Médicas ({citas.length})
         </Typography>
         <TableContainer>
@@ -132,7 +114,6 @@ export default function Citas() {
                 <TableCell align="center">Paciente</TableCell>
                 <TableCell align="center">Fecha</TableCell>
                 <TableCell align="center">Hora</TableCell>
-                <TableCell align="center">Especialidad</TableCell>
                 <TableCell align="center">Estado</TableCell>
                 <TableCell align="center">Contacto</TableCell>
                 <TableCell align="center">Notas</TableCell>
@@ -140,46 +121,85 @@ export default function Citas() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {citas.map((cita, idx) => (
-                <TableRow key={idx}>
-                  <TableCell align="center">{cita.paciente}</TableCell>
-                  <TableCell align="center">{cita.fecha}</TableCell>
-                  <TableCell align="center">{cita.hora}</TableCell>
-                  <TableCell align="center">{cita.especialidad}</TableCell>
-                  <TableCell align="center">
-                    {cita.estado === "Sin confirmar" && (
-                      <Chip label="Sin confirmar" size="small" />
-                    )}
-                    {cita.estado === "Confirmada" && (
-                      <Chip label="Confirmada" color="primary" size="small" />
-                    )}
-                    {cita.estado === "Realizada" && (
-                      <Chip label="Realizada" color="success" size="small" />
-                    )}
-                  </TableCell>
-                  <TableCell align="center">
-                    {cita.contacto}
-                    <br />
-                    <Typography variant="caption" color="text.secondary">
-                      {cita.email}
-                    </Typography>
-                  </TableCell>
-                  <TableCell align="center">{cita.notas}</TableCell>
-                  <TableCell align="center">
-                    {cita.estado === "Sin confirmar" && (
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        size="small"
-                        onClick={() => confirmarCita(idx)}
-                        sx={{ fontSize: "0.7rem" }}
-                      >
-                        Confirmar
-                      </Button>
-                    )}
+              {citas.length === 0 ? (
+                <TableRow>
+                  <TableCell
+                    colSpan={7}
+                    align="center"
+                    style={{ padding: "1.5em", backgroundColor: "#f9f9f9" }}
+                  >
+                    Todavía no hay citas programadas
                   </TableCell>
                 </TableRow>
-              ))}
+              ) : (
+                citas.map((cita, idx) => (
+                  <TableRow key={idx}>
+                    <TableCell align="center">{cita.paciente}</TableCell>
+                    <TableCell align="center">{cita.fecha}</TableCell>
+                    <TableCell align="center">{cita.hora}</TableCell>
+                    <TableCell align="center">
+                      {cita.estado === "pendiente" && (
+                        <Chip label="Sin confirmar" size="small" />
+                      )}
+                      {cita.estado === "Confirmada" && (
+                        <Chip label="Confirmada" color="primary" size="small" />
+                      )}
+                      {cita.estado === "Realizada" && (
+                        <Chip label="Realizada" color="success" size="small" />
+                      )}
+                    </TableCell>
+                    <TableCell align="center">
+                      {cita.contacto}
+                      <br />
+                      <Typography variant="caption" color="text.secondary">
+                        {cita.email}
+                      </Typography>
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      sx={{
+                        maxWidth: "5em",
+                        overflowX: "scroll",
+                        boxSizing: "border-box",
+                      }}
+                    >
+                      {cita.motivo}
+                    </TableCell>
+                    <TableCell align="center">
+                      {cita.estado === "pendiente" && (
+                        <span
+                          style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: "0.5rem",
+                            justifyContent: "center",
+                          }}
+                        >
+                          <Button
+                            variant="contained"
+                            color="success"
+                            size="small"
+                            onClick={() => confirmarCita(idx)}
+                            sx={{ fontSize: "0.7rem" }}
+                          >
+                            Confirmar
+                          </Button>
+
+                          <Button
+                            variant="contained"
+                            color="error"
+                            size="small"
+                            onClick={() => confirmarCita(idx)}
+                            sx={{ fontSize: "0.7rem" }}
+                          >
+                            Cancelar
+                          </Button>
+                        </span>
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))
+              )}
             </TableBody>
           </Table>
         </TableContainer>
